5292ec66830629ab2465117c424c94a996bf0955
Yousef Elsayed <yousefe.elsayed@gmail.com>
2023-09-14 00:41:28 +0300

    Init the matches views

---
+++ FootballLeague/FootballLeague/Presentation/TeamMatches/Views/TeamMatchesRaw.swift
@@ -0,0 +1,20 @@
+//
+//  TeamMatchesRaw.swift
+//  FootballLeague
+//
+//  Created by Yousef Elsayed on 14/09/2023.
+//
+
+import SwiftUI
+
+struct TeamMatchesRaw: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TeamMatchesRaw_Previews: PreviewProvider {
+    static var previews: some View {
+        TeamMatchesRaw()
+    }
+}
---
+++ FootballLeague/FootballLeague/Presentation/TeamMatches/Views/TeamMatchesView.swift
@@ -0,0 +1,20 @@
+//
+//  TeamMatchesView.swift
+//  FootballLeague
+//
+//  Created by Yousef Elsayed on 14/09/2023.
+//
+
+import SwiftUI
+
+struct TeamMatchesView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct TeamMatchesView_Previews: PreviewProvider {
+    static var previews: some View {
+        TeamMatchesView()
+    }
+}
